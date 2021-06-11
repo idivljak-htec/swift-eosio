844a056ba138b40eb31c66cdf1f5d164088b7c02
idivljak-htec <78956534+idivljak-htec@users.noreply.github.com>
2021-06-11 14:17:55 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,13 +8,12 @@ let package = Package(
         .library(name: "EOSIO", targets: ["EOSIO"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/greymass/secp256k1.git", .branch("master")),
         .package(url: "https://github.com/jnordberg/swift-query-string-coder.git", .branch("master")),
     ],
     targets: [
         .target(
             name: "EOSIO",
-            dependencies: ["secp256k1", "CCrypto", "QueryStringCoder"]
+            dependencies: ["CCrypto", "QueryStringCoder"]
         ),
         .target(
             name: "CCrypto"
