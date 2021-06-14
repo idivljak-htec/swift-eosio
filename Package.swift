61fc2648a585fab8e35401c61d5e94a46943bdf6
Ivan Divljak <idivljak@execom.eu>
2021-06-14 15:13:08 +0200

    POdspec updated

--- Package.swift
+++ Package.swift
@@ -8,12 +8,13 @@ let package = Package(
         .library(name: "EOSIO", targets: ["EOSIO"]),
     ],
     dependencies: [
+	.package(url: "https://github.com/greymass/secp256k1.git", .branch("master")),
         .package(url: "https://github.com/jnordberg/swift-query-string-coder.git", .branch("master")),
     ],
     targets: [
         .target(
             name: "EOSIO",
-            dependencies: ["CCrypto", "QueryStringCoder"]
+            dependencies: ["secp256k1V2", "CCrypto", "QueryStringCoder"]
         ),
         .target(
             name: "CCrypto"
